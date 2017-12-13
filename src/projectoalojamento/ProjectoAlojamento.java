3d58ca680e0d9371a5906cfb7a66fd95494813dc
Gustavo Vieira <Gustavo Vieira@DESKTOP-0338CRG>
2017-12-13 15:09:32 +0000

    Revert "v1.1 - Teste"
    This reverts commit 036e326dceec1cf93635399115ad9961b88027c0.

--- src/projectoalojamento/ProjectoAlojamento.java
+++ src/projectoalojamento/ProjectoAlojamento.java
@@ -15,7 +15,7 @@ public class ProjectoAlojamento {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+        // TODO code application logic here
     }
     
 }
